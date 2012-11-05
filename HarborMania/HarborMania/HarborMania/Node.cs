64456acc604c91dce20aba34160476ace314b2b7
fernandojordan.92 <fernandojordan.92@gmail.com>
2012-11-05 10:18:07 +0000

    Tambahan pilihan level2

--- HarborMania/HarborMania/HarborMania/Node.cs
+++ HarborMania/HarborMania/HarborMania/Node.cs
@@ -2,18 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.IO;
 
 namespace HarborMania
 {
-    class Node : Microsoft.Xna.Framework.DrawableGameComponent
+    class Node : Microsoft.Xna.Framework.GameComponent
     {
         int status; //0 kosong, 1 isi, 2 dll
         Vector2 size;
         Vector2 position;
-        Texture2D gambar;
         //GraphicsDevice gd;
 
         public int Status
@@ -38,8 +37,6 @@ namespace HarborMania
         {
             position = new Vector2();
             size = new Vector2();
-
-            LoadContent();
         }
 
         public override void Initialize()
@@ -47,20 +44,5 @@ namespace HarborMania
             base.Initialize();
         }
 
-        public void LoadContent()
-        {
-            gambar = Game.Content.Load<Texture2D>("");
-            base.LoadContent();
-        }
-
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-            base.Update(gameTime);
-        }
-
-        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-            base.Draw(gameTime);
-        }
     }
 }
--- HarborMania/HarborMania/HarborMania/Sea.cs
+++ HarborMania/HarborMania/HarborMania/Sea.cs
@@ -2,16 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace HarborMania
 {
-    class Sea : Microsoft.Xna.Framework.GameComponent
+    class Sea : Microsoft.Xna.Framework.DrawableGameComponent
     {
         Node [][] Tile;
         int WidthPerTile;
         int HeightPerTile;
         int totalNodeX;
         int totalNodeY;
+        Dictionary<int, Texture2D> mapstatus;
 
         public override void Initialize()
         {
@@ -51,6 +56,31 @@ namespace HarborMania
                 }
             }
 
+        }
+
+        public void LoadContent(int level, String path)
+        {
+            Stream streampath = TitleContainer.OpenStream(path);
+            StreamReader loadpath = new StreamReader(streampath);
+
+            for (int i = 0; i < totalNodeY; ++i)
+            {
+                String line = loadpath.ReadLine();
+                String [] splitline = line.Split(' ');
+                for (int j = 0; j < totalNodeX; ++j)
+                {
+                    Tile[i][j].Status = Convert.ToInt16(splitline[j]);
+                }
+            }
+
+            String templine = loadpath.ReadLine();
+            while (templine != null)
+            {
+                String[] splitline = templine.Split(' ');
+                if (splitline.Count() > 0)
+                    mapstatus.Add(Convert.ToInt32(splitline[0]), Game.Content.Load<Texture2D>(splitline[1]));
+                templine = loadpath.ReadLine();
+            }
         }
     }
 }
