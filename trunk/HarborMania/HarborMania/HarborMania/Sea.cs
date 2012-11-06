b4c48437dae9f0e5041b7676b0fbcab1af3c4073
fernandojordan.92@gmail.com <fernandojordan.92@gmail.com@173dc1de-125c-f4c3-4f0e-356ba684b961>
2012-11-06 06:33:08 +0000

    tambahan

--- trunk/HarborMania/HarborMania/HarborMania/Sea.cs
+++ trunk/HarborMania/HarborMania/HarborMania/Sea.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.IO;
@@ -16,14 +17,19 @@ namespace HarborMania
         int HeightPerTile;
         int totalNodeX;
         int totalNodeY;
+        int outPosY;
+        Texture2D seaTile;
+        Texture2D woodTile;
+
         Dictionary<int, Texture2D> mapstatus;
+        String path;
 
         public override void Initialize()
         {
             base.Initialize();
         }
 
-        public Sea(Game1 game) : base(game)
+        public Sea(Game game) : base(game)
         {
             WidthPerTile = 80;
             HeightPerTile = 80;
@@ -40,8 +46,9 @@ namespace HarborMania
             }
         }
 
-        public Sea(Game1 game, int W, int H, int totalNodeX_, int totalNodeY_) : base(game)
+        public Sea(Game game, int W, int H, int totalNodeX_, int totalNodeY_, String Path) : base(game)
         {
+            path = Path;
             WidthPerTile = W;
             HeightPerTile = H;
             totalNodeX = totalNodeX_;
@@ -55,13 +62,22 @@ namespace HarborMania
                     Tile[i][j] = new Node(game);
                 }
             }
-
         }
 
-        public void LoadContent(int level, String path)
+        protected override void LoadContent()
         {
+            base.LoadContent();
+        }
+
+        public void LoadContent(out List<Boat> boats)
+        {
+            seaTile = Game.Content.Load<Texture2D>("Tile/Sea");
+            woodTile = Game.Content.Load<Texture2D>("Tile/Wood");
+
             Stream streampath = TitleContainer.OpenStream(path);
             StreamReader loadpath = new StreamReader(streampath);
+
+            mapstatus = new Dictionary<int, Texture2D>();
 
             for (int i = 0; i < totalNodeY; ++i)
             {
@@ -74,13 +90,45 @@ namespace HarborMania
             }
 
             String templine = loadpath.ReadLine();
+            outPosY = Convert.ToInt32(templine);
+
+            templine = loadpath.ReadLine();
             while (templine != null)
             {
                 String[] splitline = templine.Split(' ');
-                if (splitline.Count() > 0)
+                Debug.WriteLine(splitline[1]);
+                if (splitline.Count() > 1)
                     mapstatus.Add(Convert.ToInt32(splitline[0]), Game.Content.Load<Texture2D>(splitline[1]));
                 templine = loadpath.ReadLine();
             }
+
+            boats = null;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int i = 0; i < totalNodeY; ++i)
+            {
+                for (int j = 0; j < totalNodeX; ++j)
+                {
+                    spriteBatch.Draw(mapstatus[Tile[i][j].Status],new Rectangle(j*80, i*80, 80, 80),Color.White);
+                }
+            }
+
+            for (int i = 0; i < totalNodeY; ++i)
+            {
+                if (i==outPosY)
+                    spriteBatch.Draw(seaTile, new Rectangle(6 * 80, i * 80, 80, 80), Color.White);
+                else
+                    spriteBatch.Draw(woodTile, new Rectangle(6 * 80, i * 80, 80, 80), Color.White);
+            }
+            spriteBatch.End();
         }
     }
 }
